e00c4f7a5aee34341c16cda494d8f16c55087d16
Mian Ishtiaq <35779127+devbyishtiaq@users.noreply.github.com>
2025-04-15 16:46:12 +0500

    feat: added user controller

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
-
+import cors from "cors";
 const app = express();
+import cookieParser from "cookie-parser";
 
 app.use(
   cors({
@@ -13,5 +14,10 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
 
 export { app };
